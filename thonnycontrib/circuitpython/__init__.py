1fd4cdd002e1ff9d66487e325df98f63bc100163
Aivar Annamaa <aivar.annamaa@gmail.com>
2018-02-19 23:39:57 +0200

    init

---
+++ thonnycontrib/circuitpython/__init__.py
@@ -0,0 +1,14 @@
+from thonnycontrib.micropython import MicroPythonProxy, MicroPythonConfigPage
+from thonny.globals import get_workbench
+
+class CircuitPythonProxy(MicroPythonProxy):
+    @property
+    def firmware_filetypes(self):
+        return [('*.bin files', '.bin'), ('*.uf2 files', '.uf2'), ('all files', '.*')]
+
+class CircuitPythonConfigPage(MicroPythonConfigPage):
+    pass
+
+def load_early_plugin():
+    get_workbench().add_backend("CircuitPython", CircuitPythonProxy, 
+                                "CircuitPython", CircuitPythonConfigPage)
